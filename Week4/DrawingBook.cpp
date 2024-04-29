1ffeff379b3328f75c24501b9d478f80738d04ec
HoangAnhhNekk <166677028+HoangAnhhNekk@users.noreply.github.com>
2024-04-29 19:39:49 +0700

    Update DrawingBook.cpp

--- Week4/DrawingBook.cpp
+++ Week4/DrawingBook.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,p;
+    cin >>n>>p;
 
+    if(p<=n/2)
+    {
+        cout << p/2;
+    }
+
+
+    if(p>n/2)
+    {
+       if(n%2==0 && n!=p)
+       {
+        cout << (n-1-p)/2 + 1;
+       }
+
+       else cout << (n-p)/2;
+    }
+}
